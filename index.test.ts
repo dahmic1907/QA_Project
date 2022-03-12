98e6b9a6169fe07b6f1ea6e3a1f12a6e281a1fbd
Dzeneta Ahmic <dahmic3@etf.unsa.ba>
2022-03-12 01:12:51 +0100

    Several tests added

--- index.test.ts
+++ index.test.ts
@@ -5,11 +5,9 @@ const { isAwaitExpression } = require('typescript');
 require('selenium-webdriver/chrome');
 require('selenium-webdriver/firefox');
 require('chromedriver');
-
-
 const rootURL = 'http://automationpractice.com/index.php';
 let driver = new Builder().forBrowser('chrome').build();
-//const waitUntilTime = 20000;
+
   it('waits for the driver to start', () => {
     return driver.then((_d: any) => {
       driver = _d
@@ -17,15 +15,23 @@ let driver = new Builder().forBrowser('chrome').build();
   })
   it('initialises the context', () => {
      driver.manage().window().setPosition(0, 0);
-     driver.manage().window().setSize(1050, 1024);
+     driver.manage().window().maximize();
      driver.get(rootURL);
   })
   describe('Tests - home page', () => {
-    /*afterEach(() => {
-       driver.sleep(500);
-       driver.findElement(By.className('icon-home')).click();
-    });*/
-    it('User searches for a blouse', async  () =>{
+    it('Check that cart is empty when new user launches the app', async () =>{
+      await driver.sleep(1000);
+      let text = await driver.findElement(By.className('ajax_cart_no_product')).getText();
+      expect(text).toEqual("(empty)");
+      await driver.sleep(500);  
+      await driver.findElement(By.xpath('/html/body/div/div[1]/header/div[3]/div/div/div[3]/div/a')).click();
+      await driver.sleep(1000);
+      var result = await driver.findElement(By.className('alert alert-warning')).getText();
+      await driver.sleep(500);
+      expect(result).toEqual("Your shopping cart is empty.");
+      await driver.sleep(500);
+    });
+    it('Check Search option by inserting Blouse item', async  () =>{
       await driver.sleep(5000);
       let searchBox = await driver.findElement(By.name('search_query'));
       let searchButton = await driver.findElement(By.name('submit_search'));
@@ -41,7 +47,7 @@ let driver = new Builder().forBrowser('chrome').build();
       await driver.sleep(500);
     })
     
-    it('Redirected to the Women page by clicking the Women button', async  () =>{
+    it('Redirecting to the Women page by clicking the Women button', async  () =>{
       await driver.sleep(500);
       let WomenButton = await driver.findElement(By.className('sf-with-ul'));
       await WomenButton.click();
@@ -49,7 +55,28 @@ let driver = new Builder().forBrowser('chrome').build();
       expect(result).toEqual("Women");
       await driver.sleep(500);
     })
-    it('Redirected to the Contact us page by clicking the Contact us button', async  () =>{
+
+    it('Redirecting to the Dresses page by clicking the Dresses button', async  () =>{
+      await driver.sleep(500);
+      let dressesBtn = await driver.findElement(By.xpath('/html/body/div/div[1]/header/div[3]/div/div/div[6]/ul/li[2]/a'));
+      dressesBtn.click();
+      await driver.sleep(500);
+      let result = await driver.findElement(By.className('title_block')).getText();
+      expect(result).toEqual("DRESSES");
+      await driver.sleep(5000);  
+    })
+
+    it('Redirecting to the T-shirts page by clicking the T-shirts button', async  () =>{
+      await driver.sleep(500);
+      let dressesBtn = await driver.findElement(By.xpath('/html/body/div/div[1]/header/div[3]/div/div/div[6]/ul/li[3]/a'));
+      dressesBtn.click();
+      await driver.sleep(500);
+      let result = await driver.findElement(By.className('category-name')).getText();
+      expect(result).toEqual("T-shirts");
+      await driver.sleep(5000);  
+    })
+
+    it('Redirecting to the Contact us page by clicking the Contact us button', async  () =>{
       await driver.sleep(500);
       let ContactUs = await driver.findElement(By.id('contact-link'));
       ContactUs.click();
@@ -57,24 +84,15 @@ let driver = new Builder().forBrowser('chrome').build();
       expect(rezalt).toEqual("Contact");
       await driver.sleep(5000);  
     })
-    
-    /*it('Redirected to the Dresses page by clicking the Dresses button', async  () =>{
-      await driver.sleep(500);
-      let dressesBtn = await driver.findElement(By.className('sfHoverForce'));
-      await dressesBtn.click();
-      let result = await driver.findElement(By.className('navigation_page')).getText();
-      expect(result).toEqual("Dresses");
-      await driver.sleep(5000);  
-    })*/
     it('Check Newsletter option', async  () =>{
       await driver.sleep(500);
       await driver.findElement(By.className('icon-home')).click();
-      await driver.findElement(By.id('newsletter-input')).sendKeys('dzeny.ahmic@hotmail.www',  Key.ENTER);
+      await driver.findElement(By.id('newsletter-input')).sendKeys('dzeny.ahmic@hotmail.aaa',  Key.ENTER);
       var result = await driver.findElement(By.className('alert alert-success')).getText();
       expect(result).toEqual('Newsletter : You have successfully subscribed to this newsletter.');
       await driver.sleep(1000);
     })
-    it('Redirected to the Facebook page by clicking the Facebook icon', async  () =>{
+    it('Redirecting to the Facebook page by clicking the Facebook icon', async  () =>{
       await driver.sleep(500);
       const originalWindow = await driver.getWindowHandle();
       let FacebookBtn = await driver.findElement(By.className('facebook'));
@@ -93,7 +111,7 @@ let driver = new Builder().forBrowser('chrome').build();
       await driver.switchTo().window(originalWindow);
     });
 
-    it('Redirected to the Twitter page by clicking the Twitter icon', async  () =>{
+    it('Redirecting to the Twitter page by clicking the Twitter icon', async  () =>{
       await driver.sleep(500);
       const originalWindow = await driver.getWindowHandle();
       let TwitterBtn = await driver.findElement(By.className('twitter'));
@@ -112,7 +130,7 @@ let driver = new Builder().forBrowser('chrome').build();
       await driver.switchTo().window(originalWindow);
     });
 
-    it('Redirected to the YouTube page by clicking the YouTube icon', async  () =>{
+    it('Redirecting to the YouTube page by clicking the YouTube icon', async  () =>{
       await driver.sleep(1000);
       const originalWindow = await driver.getWindowHandle();
       let youtubeBtn = await driver.findElement(By.className('youtube'));
@@ -134,10 +152,11 @@ let driver = new Builder().forBrowser('chrome').build();
   });
  
 describe('Tests- Authentication page', () => {
+  
   it('Check sign up and sign out option', async  () =>{
     await driver.sleep(500);
     await driver.findElement(By.className('login')).click();
-    await driver.findElement(By.id('email_create')).sendKeys('dahmic@hotmail.www');
+    await driver.findElement(By.id('email_create')).sendKeys('dahmic@hotmail.cii');
     await driver.findElement(By.id('SubmitCreate')).click();
     await driver.sleep(5000);
     await driver.findElement(By.id('customer_firstname')).sendKeys('Dzeneta');
@@ -160,7 +179,30 @@ describe('Tests- Authentication page', () => {
     placeholder  = await driver.findElement(By.className('navigation_page')).getText();
     expect(placeholder).toEqual("Authentication");
   })
+
+  it('Check sign in and add to cart options for registered user', async  () =>{
+    await driver.sleep(500);
+    await driver.findElement(By.className('login')).click();
+    await driver.findElement(By.id('email')).sendKeys('dzeny.ahmic@gmail.com');
+    await driver.findElement(By.id('passwd')).sendKeys('12345');
+    await driver.findElement(By.id('SubmitLogin')).click();
+    await driver.sleep(500);
+    var link = await driver.findElement(By.xpath('/html/body/div/div[1]/header/div[3]/div/div/div[6]/ul/li[3]/a'));
+    link.click();
+    await driver.sleep(5000);
+    link = await driver.findElement(By.className('button ajax_add_to_cart_button btn btn-default'));
+    link.click();
+    await driver.sleep(500);
+    await driver.findElement(By.className('cross')).click();
+    await driver.sleep(5000);
+    await driver.findElement(By.className('icon-trash')).click();
+    await driver.sleep(5000);
+    var result = await driver.findElement(By.className('alert alert-warning')).getText();
+    await driver.sleep(500);
+    expect(result).toEqual("Your shopping cart is empty.");
+    await driver.findElement(By.className('logout')).click();
+  })
 });
 afterAll(() => {
-   driver.quit();
+  driver.quit();
 });
